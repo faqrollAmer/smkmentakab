c6ac0958a2c7837072850e123b43db50fb8b6cbc
Faqrollamer <faqroll@gmail.com>
2026-03-23 04:29:17 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBzq9oIDQPX4OKykAxhceSTkovWWI7BNjM",
+  authDomain: "smkmentakab-d70cc.firebaseapp.com",
+  projectId: "smkmentakab-d70cc",
+  storageBucket: "smkmentakab-d70cc.firebasestorage.app",
+  messagingSenderId: "820684276928",
+  appId: "1:820684276928:web:82aca9b963f6e1f00bd925"
